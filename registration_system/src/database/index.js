a2b0de8841c541ca5552ceaddeb20e0431d25ded
Jairo Doni <jairo.doni97@gmail.com>
2020-09-01 22:24:09 -0300

    recuperação de senha

--- registration_system/src/database/index.js
+++ registration_system/src/database/index.js
@@ -5,6 +5,7 @@ mongoose.connect(process.env.SERVER_MONGODB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex:true,
+  useFindAndModify: false,
 });
 mongoose.Promise = global.Promise;
 
--- registration_system/src/index.js
+++ registration_system/src/index.js
@@ -7,9 +7,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('dotenv/config');
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
 
+require('./app/controllers/index')(app);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`[*] Server running on port: ${process.env.SERVER_PORT}`);
